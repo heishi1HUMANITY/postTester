7e56b27aec0a9f2dea6d119f46f51446e5c5452a
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2022-04-19 20:32:02 +0900

    ginサーバを追加

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handler(c *gin.Context) {
+	fmt.Printf("access from %s\n", c.Request.RemoteAddr)
+	fmt.Println("Header: ")
+	for key, value := range c.Request.Header {
+		fmt.Printf("%s: %s\n", key, value)
+	}
+	buf := []byte{}
+	n, _ := c.Request.Body.Read(buf)
+	body := string(buf[:n])
+	fmt.Printf("body: %s", body)
+	c.Status(200)
+}
+
+func main() {
+	server := gin.Default()
+	server.GET("/", handler)
+	server.POST("/", handler)
+	server.Run(":8080")
+}
